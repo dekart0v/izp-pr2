4ea4e5d84c8ea680a063c17bafc07f4d66456598
dekart0v <iliaiudenkov@gmail.com>
2020-12-06 15:45:58 +0100

    2nd ver

--- sps.c
+++ sps.c
@@ -3,8 +3,8 @@
 #          Made by Ilia Iudenkov (xiuden00)        #
 #        VUT FIT 2020 (zima) - IZP (Project #2)    #
 #              06.12.2020. 23:59:59                #
-#     https://github.com/dekart0v/ #
-#     Works: #
+#     https://github.com/dekart0v/izp-pr2.git      #
+#     Works:                                       #
 ####################################################
 */
 
@@ -17,11 +17,11 @@
 
 // CONSTANTS
 #define CELL_LENGTH 200 // max amount of chars in one cell
-#define ROWS_NUM 50 // max amount of rows in table
-#define CELLS_NUM 50 // max amount of cells in 1 row
+#define ROWS_NUM 50     // max amount of rows in table
+#define CELLS_NUM 50    // max amount of cells in 1 row
 
 // FUNCTIONS
-int*** allocateTable(int cell_legth, int cells_num, int rows_num);
+int ***allocateTable(int cell_legth, int cells_num, int rows_num);
 
 // MAIN program
 int main(int argc, char *argv[])
@@ -34,18 +34,35 @@ int main(int argc, char *argv[])
         perror("Error, no such file in directory.");
     }
 
-    if (argc > 1)
+    if (argc > 1) // main cycle for arguement checking (TODO)
     {
-        
-    }
+        int ***tab = allocateTable(CELL_LENGTH, CELLS_NUM, ROWS_NUM); // creating contant array
+
+        // TODO
+        // FUNCTIONS TO OPERATE WITH TABLE CONTENTS
+
+        free(tab); // free RAM for content array
+  }
 
     // SAVING CHANGES (closing file)
     fclose(input_file);
     return 0;
 }
 
-// FUNCTION ALLOCATING RAM FOR 3-DIMENSIANAL ARRAY
-int*** allocateTable(int cell_legth, int cells_num, int rows_num)
+// FUNCTION ALLOCATING RAM FOR 3-DIMENSIANAL ARRAY (TODO make dynamical)
+int ***allocateTable(int cell_legth, int cells_num, int rows_num)
 {
+    int ***table; // init 3D array
 
+    table = (int***)malloc(cell_legth * sizeof(char)); // allocate RAM for chars in cells
+    for(int i = 0; i < cell_legth; i++)
+    {
+        table[i] = (int**)malloc(cells_num * sizeof(int)); // allocate RAM for each cell in a row
+        for(int j = 0; j < cells_num; j++)
+        {
+            table[i][j] = (int*)malloc(rows_num * sizeof(int)); // allocate RAM for each row in a table
+        }
+    }
+
+    return table; 
 }
